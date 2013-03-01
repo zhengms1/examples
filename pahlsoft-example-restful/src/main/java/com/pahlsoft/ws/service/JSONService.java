5afd78a3c84fff519940cb0de3728861a563c0e2
AJ Pahl <ajpahl1008@gmail.com>
2013-02-28 20:47:26 -0500

    Added some depth to the URL logic. Put an override method for the getter part of the service.

--- pahlsoft-example-restful/src/main/java/com/pahlsoft/ws/service/JSONService.java
+++ pahlsoft-example-restful/src/main/java/com/pahlsoft/ws/service/JSONService.java
@@ -1,9 +1,13 @@
 package com.pahlsoft.ws.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -14,14 +18,41 @@ public class JSONService {
  
 	@GET
 	@Path("/get")
+	@Produces("text/html")
+	public String getProductInJSON() {
+		String output = "No Product Specified.";
+		return output;
+	}
+	
+	
+	
+	@GET
+	@Path("/get/{param}")
 	@Produces("application/json")
-	public Product getProductInJSON() {
- 
-		Product product = new Product();
-		product.setName("iPad 3");
-		product.setQty(999);
- 
-		return product; 
+	public List<Product> getProductInJSON(@PathParam("param") int productId) {
+		List<Product> pList = new ArrayList<Product>();
+		
+		if (productId==1) {
+			Product product1 = new Product();
+			product1.setName("iPad 3");
+			product1.setQty(999);
+			pList.add(product1);
+		
+		} else if (productId==2) {
+			Product product2 = new Product();
+			product2.setName("iPad 3");
+			product2.setQty(999);
+			pList.add(product2);
+			
+		} else {
+			Product product = new Product();
+			product.setName("no product specified");
+			product.setQty(000);
+			pList.add(product);
+		  
+		}
+		
+		return pList; 
  
 	}
  
