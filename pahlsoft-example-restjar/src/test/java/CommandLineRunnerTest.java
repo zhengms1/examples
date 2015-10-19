d4e7a030327c2bf1eab28fb94a3ca08883d7a9c9
ajpahl1008 <ajpahl1008@gmail.com>
2015-10-18 20:53:57 -0400

    Updated Unit for CommandLineRunner Class

--- pahlsoft-example-restjar/src/test/java/CommandLineRunnerTest.java
+++ pahlsoft-example-restjar/src/test/java/CommandLineRunnerTest.java
@@ -8,13 +8,8 @@ import org.testng.annotations.Test;
 public class CommandLineRunnerTest extends TestNG {
     @Test
     public void testCommandLine() {
-        JSONObject sample = CommandLineRunner.execute("echo","echo 'hello world'");
-        Assert.assertEquals("hello world", sample.get("echo"));
+        JSONObject sample = CommandLineRunner.execute("echo","echo hello");
+        Assert.assertEquals("[\"hello\"]", sample.get("echo").toString());
     }
 
-    @Test
-    public void testHostName() {
-        JSONObject sample = CommandLineRunner.execute("hostname","hostname");
-        Assert.assertEquals("", sample.getString("hostname"));
-    }
 }
